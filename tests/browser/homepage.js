8be42105fe1cacfa3f920ed6f0913c9c6e929c62
Gerben Meyer <g.meyer@zoover.nl>
2016-01-20 16:13:48 +0100

    fixed the browser test

--- tests/browser/homepage.js
+++ tests/browser/homepage.js
@@ -3,10 +3,9 @@ const Browser = require('zombie');
 const browser = new Browser({site: 'http://localhost:8081'});
 
 describe('User visits home page', function() {
-  before(function(done) {
-    browser.visit('/', done);
+  it('should be successful', function() {
+    browser.visit('/', function() {
+      browser.assert.success();
+    });
   });
-  // it('should be successful', function() {
-  //   browser.assert.success();
-  // });
 });
